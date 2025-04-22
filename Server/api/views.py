7532a06c7f217ed57bdd762f0788d03b8e6e7a01
Daicx <daicx135246@163.com>
2025-04-22 22:07:52 +0800

    Update views.py

--- Server/api/views.py
+++ Server/api/views.py
@@ -1,1 +1,39 @@
 
+from rest_framework import generics
+from .models import *
+from .serializers import *
+
+# 学生管理
+class StudentListCreate(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+# 教师管理
+class TeacherListCreate(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+# 课程管理
+class CourseListCreate(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+# 课程表查询
+class StudentScheduleView(generics.RetrieveAPIView):
+    serializer_class = CourseSerializer
+    
+    def get_queryset(self):
+        student_id = self.kwargs['pk']
+        return Student.objects.get(pk=student_id).courses.all()
