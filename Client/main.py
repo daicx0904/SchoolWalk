b963294311633898e22557d4cfc37a1d2d810555
Daicx <daicx135246@163.com>
2025-04-22 22:08:31 +0800

    Update main.py

--- Client/main.py
+++ Client/main.py
@@ -1,1 +1,21 @@
 
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from screens.students import StudentManagementScreen
+from screens.teachers import TeacherManagementScreen
+
+Builder.load_file('main.kv')
+
+class MainWindow(ScreenManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(StudentManagementScreen(name='students'))
+        self.add_widget(TeacherManagementScreen(name='teachers'))
+
+class StudentManagementApp(App):
+    def build(self):
+        return MainWindow()
+
+if __name__ == '__main__':
+    StudentManagementApp().run()
