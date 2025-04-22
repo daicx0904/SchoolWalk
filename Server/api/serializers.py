8136d8cb08285c7d79f57cee6363de524e73aca1
Daicx <daicx135246@163.com>
2025-04-22 22:07:29 +0800

    Update serializers.py

--- Server/api/serializers.py
+++ Server/api/serializers.py
@@ -1,1 +1,29 @@
 
+from rest_framework import serializers
+from .models import *
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    courses = CourseSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class TeacherSerializer(serializers.ModelSerializer):
+    office_location = serializers.JSONField(
+        source='office_location.coords',
+        read_only=True
+    )
+    courses = CourseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+        extra_kwargs = {
+            'office_location': {'write_only': True}
+        }
