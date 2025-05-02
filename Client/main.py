a9cee205a49b3b277bedd854d3d612059c9ea161
asus <hhyhhy2009012021@163.com>
2025-05-02 13:59:03 +0800

    1

--- Client/main.py
+++ Client/main.py
@@ -1,21 +1,59 @@
-
+import sys
+import os
+import ctypes
+import threading
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import Builder
-from screens.students import StudentManagementScreen
-from screens.teachers import TeacherManagementScreen
+from kivy.uix.boxlayout import BoxLayout
+from cefpython3 import cefpython as cef
+from kivy.clock import Clock
+Clock.schedule_once(lambda dt: threading.Thread(...).start())
 
-Builder.load_file('main.kv')
 
-class MainWindow(ScreenManager):
+class CefBrowser(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_widget(StudentManagementScreen(name='students'))
-        self.add_widget(TeacherManagementScreen(name='teachers'))
+        self.start_cef()
 
-class StudentManagementApp(App):
+    def start_cef(self):
+        def _start_cef():
+            # 设置CEF环境变量
+            os.environ['CEF_PYTHON'] = "1"
+            settings = {
+                "windowless_rendering_enabled": True,
+                "context_menu": {
+                    "enabled": False
+                }
+            }
+            cef.Initialize(settings=settings)
+
+            window_info = cef.WindowInfo()
+            window_info.SetAsChild(
+                self.get_window_handle(),
+                [0, 0, self.width, self.height]
+            )
+            cef.CreateBrowserSync(
+                window_info,
+                url="https://www.openstreetmap.org"
+            )
+            cef.MessageLoop()
+
+        threading.Thread(target=_start_cef, daemon=True).start()
+
+    def get_window_handle(self):
+        if sys.platform == 'win32':
+            # 解决Windows高DPI问题
+            ctypes.windll.user32.SetProcessDPIAware()
+            return ctypes.windll.user32.GetParent(self.get_system_window())
+        return 0
+
+
+class MyApp(App):
     def build(self):
-        return MainWindow()
+        return CefBrowser()
+
 
 if __name__ == '__main__':
-    StudentManagementApp().run()
+    # 确保Kivy配置正确
+    os.environ['KIVY_NO_CONSOLELOG'] = '1'
+    os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+    MyApp().run()
